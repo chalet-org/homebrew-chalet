934ad7544102ed2612f418f768c6464b0529dd64
Andrew King <pepsiotaku@gmail.com>
2024-10-11 22:59:39 -0400

    use plain rb file

--- chalet.rb
+++ chalet.rb
@@ -1,9 +1,9 @@
 # Chalet Homebrew Cask (WIP)
 #
 cask "chalet" do
-	version "${version}"
-	sha256 arm: "${sha_arm}",
-	       intel: "${sha_x64}"
+	version "0.7.24"
+	sha256 arm: "8990ae69516ac1a1e345aef6f5828a56fe2cca4352a07203ee8916cec8c72a90",
+	       intel: "36de3b00fb6e2f72ebc398d38817a0afadca35a9eeed63aa0901472ea27a795c"
 	arch arm: "arm64",
 	     intel: "x86_64"
 
